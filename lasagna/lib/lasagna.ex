5a8bc0a80732375b1b35d6b7d3f5bd9077cf4924
Fernando Almeida <fernando@fernandoalmeida.net>
2022-03-06 16:16:31 -0300

    Add Lasagna exercise

---
+++ lasagna/lib/lasagna.ex
@@ -0,0 +1,11 @@
+defmodule Lasagna do
+  # Please define the 'expected_minutes_in_oven/0' function
+
+  # Please define the 'remaining_minutes_in_oven/1' function
+
+  # Please define the 'preparation_time_in_minutes/1' function
+
+  # Please define the 'total_time_in_minutes/2' function
+
+  # Please define the 'alarm/0' function
+end
