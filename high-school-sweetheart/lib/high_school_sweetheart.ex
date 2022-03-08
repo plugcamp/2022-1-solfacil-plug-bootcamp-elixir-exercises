64224334863dcc330f30b946a1d35f57bb45c5f7
Fernando Almeida <fernando@fernandoalmeida.net>
2022-03-08 08:31:05 -0300

    Add High School Sweetheart exercise

---
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,32 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    # Please implement the first_letter/1 function
+  end
+
+  def initial(name) do
+    # Please implement the initial/1 function
+  end
+
+  def initials(full_name) do
+    # Please implement the initials/1 function
+  end
+
+  def pair(full_name1, full_name2) do
+    #      ******       ******
+    #    **      **   **      **
+    #  **         ** **         **
+    # **            *            **
+    # **                         **
+    # **     X. X.  +  X. X.     **
+    #  **                       **
+    #    **                   **
+    #      **               **
+    #        **           **
+    #          **       **
+    #            **   **
+    #              ***
+    #               *
+
+    # Please implement the pair/2 function
+  end
+end
