1d38d0b6b11d2994e5af5ac422b23b0c3134b2db
Fernando Almeida <fernando@fernandoalmeida.net>
2022-03-10 13:13:52 -0300

    Add Kitchen Calculator exercise

---
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -0,0 +1,17 @@
+defmodule KitchenCalculator do
+  def get_volume(volume_pair) do
+    # Please implement the get_volume/1 function
+  end
+
+  def to_milliliter(volume_pair) do
+    # Please implement the to_milliliter/1 functions
+  end
+
+  def from_milliliter(volume_pair, unit) do
+    # Please implement the from_milliliter/2 functions
+  end
+
+  def convert(volume_pair, unit) do
+    # Please implement the convert/2 function
+  end
+end
